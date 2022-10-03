4470a982429e540d3cd54ae21e25db6e36142c3a
Agustin Sampayo <agustines82@gmail.com>
2022-10-03 19:12:23 -0300

    linkeo con boostrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Formulario from "./components/Formulario";
 import Titulo from "./components/Titulo";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
     return (
