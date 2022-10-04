5cc318a97779920d1c1bce1d612fc5ab14d3b26c
Agustin Sampayo <agustines82@gmail.com>
2022-10-04 17:55:05 -0300

    todo maquetado

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -9,18 +9,7 @@ const Noticia = () => {
                     <Card.Body>
                         <Card.Title>Card Title</Card.Title>
                         <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </aside>
-
-            <aside className="col-12 col-md-4 mt-3 bg-ligth">
-                <Card className="my-2">
-                    <Card.Img variant="top" className="altura" src="https://images.ole.com.ar/2021/12/01/j28xZkI5k_2000x1500__1.jpg" />
-                    <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
-                        <Card.Text>Some quick example text to build on the card title and make up the bulk of the card's content.</Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary">Leer articulo completo</Button>
                     </Card.Body>
                 </Card>
             </aside>
